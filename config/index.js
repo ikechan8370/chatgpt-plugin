5a603d65efba08672effaa0c309a6852d6d47d6a
ikechan8370 <geyinchibuaa@gmail.com>
2023-01-16 19:26:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,8 +10,8 @@ export const Config = {
   token: SESSION_TOKEN,
   cfClearance: CF_CLEARANCE,
   proxy: PROXY,
-  username: 'geyinchi@foxmail.com',
-  password: 'Aa699008',
+  username: '',
+  password: '',
   // 改为true后，全局默认以图片形式回复，并自动发出Continue命令补全回答
   defaultUsePicture: false,
   // 每个人发起的对话保留时长。超过这个时长没有进行对话，再进行对话将开启新的对话。单位：秒
