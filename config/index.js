4a81d68c6e15d3d2e2d3990246e20eccefad1a05
ikechan8370 <geyinchibuaa@gmail.com>
2023-02-11 12:58:34 +0800

    fix: Update latest ChatGPT model

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ const PROXY = ''
 const API_KEY = ''
 
 export const Config = {
-  // 模型名称。如无特殊需求保持默认即可，会使用chatgpt-api库提供的当前可用的最适合的默认值。
-  model: '',
+  // 模型名称。如无特殊需求保持默认即可，会使用chatgpt-api库提供的当前可用的最适合的默认值。保底可用的是 text-davinci-003。当发现新的可用的chatGPT模型会更新这里的值
+  model: 'text-chat-davinci-002-sh-alpha-aoruigiofdj83',
   // 如果回答包括屏蔽词，就不返回。例如：'屏蔽词1,屏蔽词2,屏蔽词3'
   blockWords: '',
   apiKey: API_KEY,
@@ -14,11 +14,11 @@ export const Config = {
   // 每个人发起的对话保留时长。超过这个时长没有进行对话，再进行对话将开启新的对话。单位：秒
   conversationPreserveTime: 0,
   // 触发方式 可选值：at 或 prefix 。at模式下只有at机器人才会回复。prefix模式下不需要at，但需要添加前缀#chat
-  toggleMode: 'at',
+  toggleMode: 'prefix',
   // 默认完整值：`You are ${this._assistantLabel}, a large language model trained by OpenAI. You answer as concisely as possible for each response (e.g. don’t be verbose). It is very important that you answer as concisely as possible, so please remember this. If you are generating a list, do not have too many items. Keep the number of items short. Current date: ${currentDate}\n\n
   // 此项配置会覆盖掉中间部分。保持为空将使用网友从对话中推测出的指令。
   // 你可以在这里写入你希望AI回答的风格，比如希望优先回答中文，回答长一点等
-  promptPrefixOverride: '',
+  promptPrefixOverride: 'Your answer shouldn\'t be too verbose. If you are generating a list, do not have too many items. Keep the number of items short. Prefer to answer in Chinese.',
   // AI认为的自己的名字，当你问他你是谁是他会回答这里的名字。
   assistantLabel: 'ChatGPT'
 }
