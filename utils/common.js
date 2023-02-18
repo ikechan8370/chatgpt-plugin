d8265ba1d2f2865060a2b5213c9d6ba9072dcb65
ikechan8370 <geyinchibuaa@gmail.com>
2023-02-18 12:35:32 +0800

    fix: remove useless code

--- utils/common.js
+++ utils/common.js
@@ -1,11 +1,11 @@
-import { remark } from 'remark'
-import stripMarkdown from 'strip-markdown'
-export function markdownToText (markdown) {
-  return remark()
-    .use(stripMarkdown)
-    .processSync(markdown ?? '')
-    .toString()
-}
+// import { remark } from 'remark'
+// import stripMarkdown from 'strip-markdown'
+// export function markdownToText (markdown) {
+//  return remark()
+//    .use(stripMarkdown)
+//    .processSync(markdown ?? '')
+//    .toString()
+// }
 
 export async function upsertMessage (message) {
   await redis.set(`CHATGPT:MESSAGE:${message.id}`, JSON.stringify(message))
