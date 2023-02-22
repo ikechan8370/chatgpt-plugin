1bbaf8114d832235a98ac9c7f89fbd3aec28fc16
Alcedo <zyc404@gmail.com>
2023-02-22 16:41:58 +0800

    补充遗漏的默认配置

--- utils/config.js
+++ utils/config.js
@@ -21,6 +21,7 @@ const defaultConfig = {
   thinkingTips: true,
   username: '',
   password: '',
+  UA: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
   headless: false,
   chromePath: '',
   '2captchaToken': '',
