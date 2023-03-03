a45ae14a5c76b44d685f41d43c2f282421d1a44c
ikechan8370 <geyinchibuaa@gmail.com>
2023-03-03 11:05:23 +0800

    fix: 修改版本号

--- utils/config.js
+++ utils/config.js
@@ -34,7 +34,7 @@ const defaultConfig = {
   debug: true,
   defaultTimeoutMs: 120000,
   chromeTimeoutMS: 120000,
-  version: '2.0.9'
+  version: '2.0.10'
 }
 const _path = process.cwd()
 let config = {}
