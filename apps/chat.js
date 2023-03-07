7910110a6fd89fed8548d8578d8aacb52d56f565
ikechan8370 <geyinchibuaa@gmail.com>
2023-03-07 22:48:55 +0800

    fix: 增加chat优先级避免被土块抢指令

--- apps/chat.js
+++ apps/chat.js
@@ -70,7 +70,7 @@ export class chatgpt extends plugin {
       /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
       /** 优先级，数字越小等级越高 */
-      priority: 15000,
+      priority: 1144,
       rule: [
         {
           /** 命令正则匹配 */
