a110fb2080940cb4776bac6011e8e50d7abbdade
ikechan8370 <geyinchibuaa@gmail.com>
2023-03-20 23:55:54 +0800

    fix: 语音太长提示不回复

--- apps/chat.js
+++ apps/chat.js
@@ -744,7 +744,7 @@ export class chatgpt extends plugin {
             }
           }
         } else {
-          await this.reply('你没有配置转语音API或者文字太长了哦，我用文本回复你吧', e.isGroup)
+          await this.reply('你没有配置转语音API或者文字太长了哦，我用文本回复你吧')
           await this.reply(`${response}`, e.isGroup)
           if (quotemessage.length > 0) {
             this.reply(await makeForwardMsg(this.e, quotemessage))
