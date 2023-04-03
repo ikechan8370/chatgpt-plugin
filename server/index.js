b45f0a92888f8622df0728cf38331ea9434712aa
Alcedo <zyc404@gmail.com>
2023-04-03 16:47:18 +0800

    改为使用api获取公网ip

--- server/index.js
+++ server/index.js
@@ -4,35 +4,28 @@ import fstatic from '@fastify/static'
 
 import fs from 'fs'
 import path from 'path'
-import os from 'os'
+import http from 'http'
 
 function getPublicIP() {
-  const ifaces = os.networkInterfaces();
-  let en0;
-
-  Object.keys(ifaces).forEach((ifname) => {
-    let alias = 0;
-
-    ifaces[ifname].forEach(function (iface) {
-      if ("IPv4" !== iface.family || iface.internal !== false) {
-        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-        return;
-      }
-
-      if (alias >= 1) {
-        // this single interface has multiple ipv4 addresses
-        en0 = iface.address;
-        console.log(ifname + ":" + alias, iface.address);
-      } else {
-        // this interface has only one ipv4 adress
-        console.log(ifname, iface.address);
-        en0 = iface.address;
-      }
-      ++alias;
+  return new Promise((resolve, reject) => {
+    http.get('http://ipinfo.io/json', (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        try {
+          const ip = JSON.parse(data).ip;
+          resolve(ip);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    }).on('error', (err) => {
+      reject(err);
     });
   });
-  return en0;
-};
+}
 
 export async function createServer() {
 const __dirname = path.resolve();
@@ -70,7 +63,7 @@ server.post('/cache', async (request, reply) => {
         const filename = body.entry + '.json';
         const filepath = path.join(dir, filename);
         const regexUrl = /\b((?:https?|ftp|file):\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|])/g;
-        const ip = getPublicIP()
+        const ip = await getPublicIP()
         try {
           fs.mkdirSync(dir, { recursive: true });
           fs.writeFileSync(filepath, JSON.stringify({
