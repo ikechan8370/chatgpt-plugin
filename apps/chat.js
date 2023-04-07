08234c6f6a0b61f47fd44ad4c6df8035520cb6d3
Alcedo <zyc404@gmail.com>
2023-04-07 15:35:56 +0800

    修正错误的变量调用

--- apps/chat.js
+++ apps/chat.js
@@ -873,7 +873,7 @@ export class chatgpt extends plugin {
           await this.reply(`出现错误：${err}`, true, { recallMsg: e.isGroup ? 10 : 0 })
         } else {
           // 这里是否还需要上传到缓存服务器呐？多半是代理服务器的问题，本地也修不了，应该不用吧。
-          await this.renderImage(e, use !== 'bing' ? 'content/ChatGPT/index' : 'content/Bing/index', `通信异常,错误信息如下 ${error?.message || error?.data?.message || (typeof(error) === 'object' ? JSON.stringify(error) : error) || '未能确认错误类型！'}`, prompt)
+          await this.renderImage(e, use !== 'bing' ? 'content/ChatGPT/index' : 'content/Bing/index', `通信异常,错误信息如下 ${err?.message || err?.data?.message || (typeof(err) === 'object' ? JSON.stringify(err) : err) || '未能确认错误类型！'}`, prompt)
         }
       }
     }
