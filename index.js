df09a0746c98e9e6e83d8f456674e2acaf220ef0
Alcedo <zyc404@gmail.com>
2023-04-10 16:31:35 +0800

    添加依赖

--- index.js
+++ index.js
@@ -31,15 +31,14 @@ for (let i in files) {
 try {
   await import('fastify')
   await import('fastify-cookie')
-  await import('fastify-cookie')
   await import('@fastify/cors')
   await import('@fastify/static')
   await import('os-utils')
-  // 启动服务器
-  await createServer()
 } catch (err) {
-  logger.warn('【ChatGPT-Plugin】依赖fastify、fastify-cookie、@fastify/cors、@fastify/static、os-utils未安装，可能影响系统Api服务运行，当前Api服务模块已禁用，建议执行pnpm install fastify @fastify/cors @fastify/static安装')
+  logger.warn('【ChatGPT-Plugin】依赖fastify、fastify-cookie、@fastify/cors、@fastify/static、os-utils未安装，可能影响系统Api服务运行，当前Api服务模块已禁用，建议执行pnpm install fastify @fastify/cors @fastify/static fastify-cookie os-utils安装')
 }
+// 启动服务器
+await createServer()
 
 logger.info('**************************************')
 logger.info('chatgpt-plugin加载成功')
