2ce74dbada1ee9e927f4ee48400b5d6c77fca646
Alcedo <zyc404@gmail.com>
2023-04-10 16:26:57 +0800

    添加配置页面接口

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import fstatic from '@fastify/static'
 import fs from 'fs'
 import path from 'path'
 import os from 'os'
+import osUtils from 'os-utils'
+import schedule from 'node-schedule'
 
 import { Config } from '../utils/config.js'
 import { randomString, getPublicIP } from '../utils/common.js'
@@ -16,6 +18,41 @@ const server = fastify({
 })
 
 let usertoken = ''
+let Statistics = {
+  SystemAccess: {
+    count: 0,
+    oldCount: 0
+  },
+  CacheFile: {
+    count: 0,
+    oldCount: 0
+  },
+  WebAccess: {
+    count: 0,
+    oldCount: 0
+  },
+  SystemLoad: {
+    count: 0,
+    oldCount: 0
+  }
+}
+
+async function getLoad() {
+  // 获取当前操作系统平台
+  const platform = os.platform();
+  // 判断平台是Linux还是Windows
+  if (platform === 'linux') {
+    // 如果是Linux，使用os.loadavg()方法获取负载平均值
+    const loadAvg = os.loadavg();
+    return loadAvg[0] * 100
+  } else if (platform === 'win32') {
+    // 如果是Windows，使用os-utils模块获取CPU使用率
+    const usage = await osUtils.cpuUsage()
+    return usage * 100
+  } else {
+    return 0
+  }
+}
 
 export async function createServer() {
   await server.register(cors, {
@@ -26,18 +63,22 @@ export async function createServer() {
   })
   await server.register(fastifyCookie)
   await server.get('/page/*', (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const stream = fs.createReadStream('plugins/chatgpt-plugin/server/static/index.html')
     reply.type('text/html').send(stream)
   })
   await server.get('/help/*', (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const stream = fs.createReadStream('plugins/chatgpt-plugin/server/static/index.html')
     reply.type('text/html').send(stream)
   })
   await server.get('/auth/*', (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const stream = fs.createReadStream('plugins/chatgpt-plugin/server/static/index.html')
     reply.type('text/html').send(stream)
   })
   await server.get('/admin/*', (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const token = request.cookies.token || 'unknown'
     if (token != usertoken) {
         reply.redirect(301, '/auth/login')
@@ -47,10 +88,12 @@ export async function createServer() {
   })
   // 登录
   server.post('/login', async (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const body = request.body || {}
     if (body.qq && body.passwd) {
       if (body.qq == Bot.uin && await redis.get('CHATGPT:ADMIN_PASSWD') == body.passwd) {
-        reply.setCookie('token', randomString(32), {path: '/admin'})
+        usertoken = randomString(32)
+        reply.setCookie('token', usertoken, {path: '/'})
         reply.send({login:true})
       } else {
         reply.send({login:false,err:'用户名密码错误'})
@@ -61,18 +104,19 @@ export async function createServer() {
   })
   // 页面数据获取
   server.post('/page', async (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const body = request.body || {}
     if (body.code) {
       const dir = 'resources/ChatGPTCache/page'
       const filename = body.code + '.json'
       const filepath = path.join(dir, filename)
-
       let data = fs.readFileSync(filepath, 'utf8')
       reply.send(data)
     }
   })
   // 帮助内容获取
   server.post('/help', async (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const body = request.body || {}
     if (body.use) {
       const dir = 'plugins/chatgpt-plugin/resources'
@@ -85,6 +129,7 @@ export async function createServer() {
   })
   // 创建页面缓存内容
   server.post('/cache', async (request, reply) => {
+    Statistics.SystemAccess.count += 1
     const body = request.body || {}
     if (body.content) {
       const dir = 'resources/ChatGPTCache/page'
@@ -108,6 +153,7 @@ export async function createServer() {
           suggest: body.content.suggest || [],
           time: new Date()
         }))
+        Statistics.CacheFile.count += 1
         reply.send({ file: body.entry, cacheUrl: `http://${ip}:${Config.serverPort || 3321}/page/${body.entry}` })
       } catch (err) {
         console.error(err)
@@ -115,6 +161,41 @@ export async function createServer() {
       }
     }
   })
+  // 获取系统状态
+  server.post('/system-statistics', async (request, reply) => {
+    Statistics.SystemAccess.count += 1
+    Statistics.SystemLoad.count = await getLoad()
+    reply.send(Statistics)
+  })
+
+  server.post('/sysconfig', async (request, reply) => {
+    Statistics.SystemAccess.count += 1
+    const token = request.cookies.token || 'unknown'
+    if (token != usertoken) {
+      reply.send({err: '未登录'})
+    } else {
+      reply.send(Config)
+    }
+  })
+  
+
+  //定时任务
+  var rule = new schedule.RecurrenceRule();
+  rule.hour = 0;
+  rule.minute = 0;
+  let job_Statistics = schedule.scheduleJob(rule, function() {
+    Statistics.SystemAccess.oldCount = Statistics.SystemAccess.count
+    Statistics.CacheFile.oldCount = Statistics.CacheFile.count
+    Statistics.WebAccess.oldCount = Statistics.WebAccess.count
+    Statistics.SystemAccess.count = 0
+    Statistics.CacheFile.count = 0
+    Statistics.WebAccess.count = 0
+  });
+  let job_Statistics_SystemLoad = schedule.scheduleJob('0 * * * *', async function(){
+    Statistics.SystemLoad.count = await getLoad()
+    Statistics.SystemLoad.oldCount = Statistics.SystemLoad.count
+  });
+  
   server.listen({
     port: Config.serverPort || 3321,
     host: '0.0.0.0'
