673fd88141dbe02b52633a0db0ad17673cda8933
ikechan8370 <geyinchibuaa@gmail.com>
2023-04-17 12:58:40 +0800

    fix: 打招呼prompt不能热更新的问题

--- utils/randomMessage.js
+++ utils/randomMessage.js
@@ -23,9 +23,9 @@ const newFetch = (url, options = {}) => {
 
   return fetch(url, mergedOptions)
 }
-const question = Config.helloPrompt || '写一段话让大家来找我聊天。类似于“有人找我聊天吗？"这种风格，轻松随意一点控制在20个字以内'
 
 export async function generateHello () {
+  let question = Config.helloPrompt || '写一段话让大家来找我聊天。类似于“有人找我聊天吗？"这种风格，轻松随意一点控制在20个字以内'
   let api = new ChatGPTAPI({
     apiBaseUrl: Config.openAiBaseUrl,
     apiKey: Config.apiKey,
