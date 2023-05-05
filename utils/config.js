6485d7a93e31c99ed840405b9cb390123ae72f3a
ikechan8370 <geyinchi@buaa.edu.cn>
2023-05-05 21:04:55 +0800

    fix: update version

--- utils/config.js
+++ utils/config.js
@@ -117,7 +117,7 @@ const defaultConfig = {
   azureTTSEmotion: false,
   enhanceAzureTTSEmotion: false,
   autoJapanese: false,
-  version: 'v2.5.8'
+  version: 'v2.5.9'
 }
 const _path = process.cwd()
 let config = {}
