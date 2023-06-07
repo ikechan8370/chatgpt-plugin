db5b0b2b9751fae8edfa886aff27d1992836550e
Alcedo <zyc404@gmail.com>
2023-06-07 15:55:26 +0800

    修复错误

--- server/index.js
+++ server/index.js
@@ -126,9 +126,9 @@ export async function createServer() {
       } else {
         const user = await getUserData(body.qq)
         if (user.passwd != '' && user.passwd === body.passwd) {
-          usertoken.push({ user: body.qq, token, autho: 'user', token: token })
+          usertoken.push({ user: body.qq, token, autho: 'user'})
           reply.setCookie('token', token, { path: '/' })
-          reply.send({ login: true, autho: 'user' })
+          reply.send({ login: true, autho: 'user', token: token })
         } else {
           reply.send({ login: false, err: `用户名密码错误,如果忘记密码请私聊机器人输入 ${body.qq == Bot.uin ? '#修改管理密码' : '#修改用户密码'} 进行修改` })
         }
