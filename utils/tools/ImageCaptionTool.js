c3d9f65a03e4eeaabaec196b0145e762d6e33cd0
ikechan8370 <geyinchi@buaa.edu.cn>
2023-06-25 15:08:47 +0800

    fix: 删掉多余的提示

--- utils/tools/ImageCaptionTool.js
+++ utils/tools/ImageCaptionTool.js
@@ -16,7 +16,7 @@ export class ImageCaptionTool extends AbstractTool {
       },
       question: {
         type: 'string',
-        description: 'if you want to ask something about this picture, write your question in English here. If you just want to know what\'s in the photo, leave it blank'
+        description: 'if you want to ask something about this picture, write your question in English here.'
       }
     },
     required: []
