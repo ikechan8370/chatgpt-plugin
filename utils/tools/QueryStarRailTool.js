905cd5b982b9acdae6c282855a6f8498f383065b
ikenxuan <112480306+ikenxuan@users.noreply.github.com>
2023-06-26 23:14:16 +0800

    优化sr tool查询

--- utils/tools/QueryStarRailTool.js
+++ utils/tools/QueryStarRailTool.js
@@ -41,48 +41,55 @@ export class QueryStarRailTool extends AbstractTool {
       }
     }
     try {
-      let { Panel } = await import('../../../StarRail-plugin/apps/panel.js')
-      e.msg = character ? `*${character}面板${uid}` : '*更新面板' + uid
-      e.user_id = qq
-      e.isSr = true
-      let panel = new Panel(e)
-      panel.e = e
-      panel.panel(e).catch(e => logger.warn(e))
-      let uidRes = await fetch('https://avocado.wiki/v1/info/' + uid)
-      uidRes = await uidRes.json()
-      let { assistAvatar, displayAvatars } = uidRes.playerDetailInfo
-      function dealAvatar (avatar) {
-        delete avatar.position
-        delete avatar.vo_tag
-        delete avatar.desc
-        delete avatar.promption
-        delete avatar.relics
-        delete avatar.behaviorList
-        delete avatar.images
-        delete avatar.ranks
-        if (avatar.equipment) {
-          avatar.equipment = {
-            level: avatar.equipment.level,
-            rank: avatar.equipment.rank,
-            name: avatar.equipment.name,
-            skill_desc: avatar.equipment.skill_desc
+      try {
+        let { Panel } = await import('../../../StarRail-plugin/apps/panel.js')
+        e.msg = character ? `*${character}面板${uid}` : '*更新面板' + uid
+        e.user_id = qq
+        e.isSr = true
+        let panel = new Panel(e)
+        panel.e = e
+        panel.panel(e).catch(e => logger.warn(e))
+
+        return `The panel has already been sent to the group`
+      } catch (e) {
+        let uidRes = await fetch('https://avocado.wiki/v1/info/' + uid)
+        uidRes = await uidRes.json()
+        let { assistAvatar, displayAvatars } = uidRes.playerDetailInfo
+        function dealAvatar(avatar) {
+          delete avatar.position
+          delete avatar.vo_tag
+          delete avatar.desc
+          delete avatar.promption
+          delete avatar.relics
+          delete avatar.behaviorList
+          delete avatar.images
+          delete avatar.ranks
+          if (avatar.equipment) {
+            avatar.equipment = {
+              level: avatar.equipment.level,
+              rank: avatar.equipment.rank,
+              name: avatar.equipment.name,
+              skill_desc: avatar.equipment.skill_desc
+            }
           }
         }
+        dealAvatar(assistAvatar)
+        if (displayAvatars) {
+          displayAvatars.forEach(avatar => {
+            dealAvatar(avatar)
+          })
+        }
+        uidRes.playerDetailInfo.assistAvatar = assistAvatar
+        uidRes.playerDetailInfo.displayAvatars = displayAvatars
+        delete uidRes.repository
+        delete uidRes.version
+        return `the player info in json format is: \n${JSON.stringify(uidRes)}`
       }
-      dealAvatar(assistAvatar)
-      if (displayAvatars) {
-        displayAvatars.forEach(avatar => {
-          dealAvatar(avatar)
-        })
-      }
-      uidRes.playerDetailInfo.assistAvatar = assistAvatar
-      uidRes.playerDetailInfo.displayAvatars = displayAvatars
-      delete uidRes.repository
-      delete uidRes.version
-      return `the player info in json format is: \n${JSON.stringify(uidRes)}`
+
     } catch (err) {
       return `failed to query, error: ${err.toString()}`
     }
+
   }
 
   description = 'Useful when you want to query player information of Honkai Star Rail(崩坏：星穹铁道). '
