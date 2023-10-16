c68a50e8bc3b3464d9556a61719030078881178f
HalcyonAlcedo <41666148+HalcyonAlcedo@users.noreply.github.com>
2023-10-16 21:49:00 +0800

    继续适配Trss (#583)
    * fix: 修复星火api上下文
    
    * 将无星火ck的情况降低为warn
    
    * feat: 添加星火设定自定义代码功能
    
    * 修复星火api模式的一些问题
    
    * 修复导出配置问题
    
    * feat:添加工具箱快捷登录接口
    
    * 添加工具箱快捷登录指令
    
    * 阻止群聊使用快捷登录
    
    * 添加Azure配置支持，修复重复的配置项冲突
    
    * 移除旧版本渲染和新版本帮助
    
    * 添加工具箱
    
    * 更新工具箱替换原有后台
    
    * 更新工具箱适配代码
    
    * 后台适配Trss
    
    * 修复trss不支持sendPrivateMsg的问题
    
    * 优化路由
    
    * 修复路由
    
    * 适配其他uin
    
    * 添加bing第三方绘图
    
    * 修复bing绘图第三方调用错误
    
    * 添加bing第三方绘图采样配置
    
    * 修复错误
    
    * 添加bing第三方绘图图片大小配置
    
    * 修复视图错误
    
    * 使用ap替换第三方绘图
    
    * 适配trss
    
    * server 适配trss
    
    * 修复错误的后台版本更新
    
    * 添加锅巴用户数据
    
    * 修复server初始化消息错误
    
    * 添加锅巴插件适配
    
    * 更新后台页面
    
    * 添加锅巴代理接口
    
    * 优化锅巴接口代理
    
    * 修复锅巴代理参数
    
    * 删除调试信息
    
    * 修复headers
    
    * 更新后台锅巴插件支持

--- server/static/assets/md5.20cf1e4a.js
+++ server/static/assets/md5.20cf1e4a.js
@@ -1,4 +1,4 @@
-import{ak as commonjsGlobal}from"./index.f985de17.js";var md5$1={exports:{}};/**
+import{ak as commonjsGlobal}from"./index.a457a291.js";var md5$1={exports:{}};/**
  * [js-md5]{@link https://github.com/emn178/js-md5}
  *
  * @namespace md5
--- utils/common.js
+++ utils/common.js
@@ -828,17 +828,17 @@ export function getMaxModelTokens (model = 'gpt-3.5-turbo') {
 
 export function getUin (e) {
   if (e?.bot?.uin) return e.bot.uin
-  if (e) {
-    if (Array.isArray(e.bot.uin)) {
-      if (Config.trssBotUin && e.bot.uin.indexOf(Config.trssBotUin) > -1) return Config.trssBotUin
-      else return e.bot.uin[0]
-    } else return e.bot.uin
-  } else {
-    if (Array.isArray(Bot.uin)) {
-      if (Config.trssBotUin && Bot.uin.indexOf(Config.trssBotUin) > -1) return Config.trssBotUin
-      else return Bot.uin[0]
-    } else return Bot.uin
-  }
+  if (Array.isArray(Bot.uin)) {
+    if (Config.trssBotUin && Bot.uin.indexOf(Config.trssBotUin) > -1) {return Config.trssBotUin}
+    else {
+        Bot.uin.forEach((u) => {
+          if (Bot[u].self_id) {
+            return Bot[u].self_id 
+          }
+        })
+        return Bot.uin[Bot.uin.length - 1]
+    }
+  } else return Bot.uin
 }
 
 /**
