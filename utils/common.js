e29e370f25cae33b053253af00652e0bdc4b075c
ikechan8370 <geyinchi@buaa.edu.cn>
2023-11-07 21:37:38 +0800

    fix: 模型maxToken更新 gpt4后续更新

--- utils/common.js
+++ utils/common.js
@@ -832,8 +832,10 @@ export function getMaxModelTokens (model = 'gpt-3.5-turbo') {
   if (model.startsWith('gpt-3.5-turbo')) {
     if (model.includes('16k')) {
       return 16000
-    } else {
+    } else if (model.includes('0613') || model.includes('0314')) {
       return 4000
+    } else {
+      return 16000
     }
   } else {
     if (model.includes('32k')) {
