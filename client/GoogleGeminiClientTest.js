0ac70a325f7251e5b06083a2d492d8db56d907a5
ikechan8370 <geyinchibuaa@gmail.com>
2023-12-15 01:53:02 +0800

    fix

--- client/GoogleGeminiClientTest.js
+++ client/GoogleGeminiClientTest.js
@@ -4,7 +4,7 @@ async function test () {
   const client = new GoogleGeminiClient({
     e: {},
     userId: 'test',
-    key: 'AIzaSyBZEC3SLp0CVDnNY8WoRT7hn0LB8zn8dFA',
+    key: '',
     model: 'gemini-pro'
   })
 }
